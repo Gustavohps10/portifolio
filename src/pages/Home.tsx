63676236f2edc8aa390f65a892434cf4e113b035
Gustavo <gustavoh.santos735@gmail.com>
2023-09-19 22:29:47 -0300

    update phone number

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -235,18 +235,18 @@ export function Home(){
               <ul>
                 <li className="title">Contate-me</li>
                 <li className="my-email"> gustavoh.santos735@gmail.com</li>
-                <li>(44) 98462-8599</li>
+                <li>(44) 98805-4126</li>
               </ul>
               <ul>
                 <li className="title">Social</li>
                 <li><a href="http://github.com/gustavohps10" target="_blank"><FontAwesomeIcon className="icon" icon={faGithub} />Github</a></li>
                 <li><a href="https://www.linkedin.com/in/gustavo-henrique-pereira-dos-santos-69a423210/" target="_blank"><FontAwesomeIcon className="icon" icon={faLinkedin} /> Linkedin</a></li>
                 <li><a href="https://discord.com/invite/wY7rhAde5x" target="_blank"><FontAwesomeIcon className="icon" icon={faDiscord} /> Discord</a></li>
-                <li><a href="http://wa.me/5544984628599" target="_blank"><FontAwesomeIcon className="icon" icon={faWhatsapp} /> Whatsapp</a></li>
+                <li><a href="http://wa.me/5544988054126" target="_blank"><FontAwesomeIcon className="icon" icon={faWhatsapp} /> Whatsapp</a></li>
               </ul>
             </div>
             <hr />
-            <span>© Made with ❤️ Gustavo Henrique 2022</span>
+            <span>© Made with ❤️ Gustavo Henrique 2023</span>
           </section>
         </div>
       </> 
