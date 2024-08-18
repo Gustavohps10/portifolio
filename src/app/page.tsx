2d2f6d1cd8b0f9c88f5142f8d1db2d2e161fcb35
Gustavo Henrique <gustavoh.santos735@gmail.com>
2024-08-17 23:59:16 -0300

    chore: config env variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
-export default function Page() {
-  return '...' // We'll update this
+export default async function Page() {
+  const res = await fetch('https://api.github.com/users/Gustavohps10/repos', {
+    next: { revalidate: 3600 },
+  })
+  const data = await res.json()
+
+  console.log(data)
+
+  return <></>
 }
